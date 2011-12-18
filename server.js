4f14ef75b3b76228e77035d97fe06331465783d6
Karl Guillotte <karl.guillotte.1@gmail.com>
2011-12-17 21:35:48 -0800

    First commit

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.error(function(err, req, res, next) {
 	res.redirect("/");
 });
 
-
-app.listen(3000);
-// console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Express server listening on port %d in %s mode", port, app.settings.env);
